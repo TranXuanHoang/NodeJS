c0527bc06fa837c75a33acfc25acb35bf05df5ec
TranXuanHoang <hoangtx.ict@gmail.com>
2020-08-09 17:38:10 +0900

    Change to a user account adding app

--- 00-basic/routes.js
+++ 00-basic/routes.js
@@ -1,52 +1,114 @@
 const fs = require('fs')
 
+// Define routes of the app
+const ROUTES = {
+  HOME: '/',
+  USERS: '/users',
+  CREATE_USER: '/create-user'
+}
+
+// Get CSS
+let css
+fs.readFile('./style.css', (err, data) => {
+  css = data.toString()
+})
+
 const requestHandler = (req, res) => {
-  console.log(req.method, req.url)
   const url = req.url
   const method = req.method
+  console.log(method, url)
 
-  if (url === '/') {
+  if (url === ROUTES.HOME) {
     res.setHeader('Content-Type', 'text/html')
-    res.write('<html>')
-    res.write('<head><title>Enter Message</title></head>')
-    res.write('<body>')
-    res.write('<h1>Enter your message</h1>')
-    res.write('<form method="POST" action="/message">')
-    res.write('<input type="text" name="message" row="4">')
-    res.write('<button type="submit">Send</button>')
-    res.write('</form>')
-    res.write('</body>')
-    res.write('</html>')
+    res.write(
+      `<html>
+      <head>
+        <title>User Portal</title>
+        <style>${css}</style>
+      </head>`
+    )
+    res.write(
+      `<body>
+        <header>
+          <nav>
+            <a href="${ROUTES.HOME}" class="active">Home</a>
+            <a href="${ROUTES.USERS}">Users</a>
+          </nav>
+          <br>
+        </header>
+        <h1>Enter your username</h1>
+        <form method="POST" action="${ROUTES.CREATE_USER}">
+          <p>
+            <input type="text" name="username" autocomplete="off">
+          </p>
+          <button type="submit">Add User</button>
+        </form>
+      </body>
+      </html>`
+    )
     return res.end()
   }
-  if (url === '/message' && method === 'POST') {
+
+  if (url === ROUTES.CREATE_USER && method === 'POST') {
     const body = [];
     req.on('data', (chunk) => {
-      console.log(chunk)
       body.push(chunk)
     })
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString()
-      const message = parsedBody.split('=')[1]
-      fs.writeFile('message.txt', message, (err) => {
-        console.error(err)
-        if (!err) {
-          res.statusCode = 302
-          res.setHeader('Location', '/')
-          return res.end()
-        }
+      const username = parsedBody.split('=')[1]
+
+      // Load current users list from file
+      fs.readFile('users.json', (err, data) => {
+        // If cannot load users data from file, consider that as the first time
+        // create user request was sent by assigning the 'users' list as an empty array
+        users = !err ? JSON.parse(data.toString()) : []
+        users.push(username)
+
+        // Save users list with new user added to file
+        fs.writeFile('users.json', JSON.stringify(users, '', 2), (err) => {
+          console.error(err)
+          if (!err) {
+            res.statusCode = 302
+            res.setHeader('Location', ROUTES.USERS)
+            return res.end()
+          }
+        })
       })
     })
   }
 
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<html>')
-  res.write('<head><title>My Website</title></head>')
-  res.write('<body>')
-  res.write('<h1>Hello World</h1>')
-  res.write('</body>')
-  res.write('</html>')
-  res.end()
+  if (url === ROUTES.USERS) {
+    // Read users data from file and return a webpage showing the users list
+    fs.readFile('users.json', (err, data) => {
+      users = !err ? JSON.parse(data.toString()) : []
+      let usersList = users.map(username => `<li>${username}</li>`).join('')
+
+      res.setHeader('Content-Type', 'text/html')
+      res.write(
+        `<html>
+        <head>
+          <title>Users</title>
+          <style>${css}</style>
+        </head>`
+      )
+      res.write(
+        `<body>
+          <header>
+            <nav>
+              <a href="${ROUTES.HOME}">Home</a>
+              <a href="${ROUTES.USERS}" class="active">Users</a>
+            </nav>
+            <br>
+          </header>
+          <h1>List of Users</h1>
+          ${users.length > 0 ? `<ol>${usersList}</ol>` : 'No users found!'}
+        </body>
+        </html>`
+      )
+      return res.end()
+    })
+  }
 }
 
 module.exports = requestHandler
