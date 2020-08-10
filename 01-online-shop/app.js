0a2eacfbf7fdb150346cd46048ae3df3caaef2cf
TranXuanHoang <hoangtx.ict@gmail.com>
2020-08-11 00:23:16 +0900

    Add CSS styles and serve them statically

--- 01-online-shop/app.js
+++ 01-online-shop/app.js
@@ -8,8 +8,10 @@ const shopRoutes = require('./routes/shop')
 
 const app = express()
 
+// Parse the request body so that the following handlers can directly read the body
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// Logout requests' info
 app.use('/', (req, res, next) => {
   console.log(`${req.method} ${req.url}`)
   if (req.method === 'POST') {
@@ -18,9 +20,14 @@ app.use('/', (req, res, next) => {
   next()
 })
 
+// Serve static contents
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Handle app's routes
 app.use(adminRoutes.ROOT_ROUTE_SEGMENT, adminRoutes.route)
 app.use(shopRoutes)
 
+// Handle 404 Not Found
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
