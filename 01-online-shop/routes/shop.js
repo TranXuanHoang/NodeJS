803221db67988aade6ecca90172517a6dc00ca06
TranXuanHoang <hoangtx.ict@gmail.com>
2020-08-13 00:31:58 +0900

    Add orders page with its controller and route logic

--- 01-online-shop/routes/shop.js
+++ 01-online-shop/routes/shop.js
@@ -7,6 +7,7 @@ const route = express.Router()
 route.get('/', shopController.getIndex)
 route.get('/products', shopController.getProducts)
 route.get('/cart', shopController.getCart)
+route.get('/orders', shopController.getOrders)
 route.get('/checkout', shopController.getCheckout)
 
 module.exports = route
