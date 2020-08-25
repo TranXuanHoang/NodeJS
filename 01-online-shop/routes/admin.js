93a595d6d92c38dcd59fd2d47ede22e959091fa3
TranXuanHoang <hoangtx.ict@gmail.com>
2020-08-25 13:19:04 +0900

    Use a middleware to protect routes
    * All routes requiring authentication are protected from
      directly loading by entering URL to browsers's address bar
    * The middleware redirects unauthorized requests to the login page

--- 01-online-shop/routes/admin.js
+++ 01-online-shop/routes/admin.js
@@ -1,14 +1,15 @@
 const express = require('express')
 
 const adminController = require('../controllers/admin')
+const isAuth = require('../middleware/is-auth')
 
 const route = express.Router()
 
-route.get('/add-product', adminController.getAddProduct)
-route.post('/add-product', adminController.postAddProduct)
-route.get('/edit-product/:productId', adminController.getEditProduct)
-route.post('/edit-product', adminController.postEditProduct)
-route.post('/delete-product', adminController.postDeleteProduct)
-route.get('/products', adminController.getProducts)
+route.get('/add-product', isAuth, adminController.getAddProduct)
+route.post('/add-product', isAuth, adminController.postAddProduct)
+route.get('/edit-product/:productId', isAuth, adminController.getEditProduct)
+route.post('/edit-product', isAuth, adminController.postEditProduct)
+route.post('/delete-product', isAuth, adminController.postDeleteProduct)
+route.get('/products', isAuth, adminController.getProducts)
 
 module.exports = route
--- 01-online-shop/routes/shop.js
+++ 01-online-shop/routes/shop.js
@@ -1,17 +1,18 @@
 const express = require('express')
 
 const shopController = require('../controllers/shop')
+const isAuth = require('../middleware/is-auth')
 
 const route = express.Router()
 
 route.get('/', shopController.getIndex)
 route.get('/products', shopController.getProducts)
 route.get('/products/:productId', shopController.getProduct)
-route.get('/cart', shopController.getCart)
-route.post('/cart', shopController.postAddToCart)
-route.post('/cart-delete-item', shopController.postCartDeleteProduct)
-route.post('/create-order', shopController.postOrder)
-route.get('/orders', shopController.getOrders)
+route.get('/cart', isAuth, shopController.getCart)
+route.post('/cart', isAuth, shopController.postAddToCart)
+route.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct)
+route.post('/create-order', isAuth, shopController.postOrder)
+route.get('/orders', isAuth, shopController.getOrders)
 // route.get('/checkout', shopController.getCheckout)
 
 module.exports = route
