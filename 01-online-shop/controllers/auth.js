2c1f20c18673364a80905dea1b0250beec1ddf5d
TranXuanHoang <hoangtx.ict@gmail.com>
2020-08-27 01:52:55 +0900

    Add a flow of reseting passwords
    * Add two new fields to the user model to save password reset token
    * Define routes, wire up controller and write view for the reset password

--- 01-online-shop/controllers/auth.js
+++ 01-online-shop/controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto')
+
 const bcrypt = require('bcryptjs')
 const sendgridMail = require('@sendgrid/mail')
 
@@ -112,11 +114,11 @@ exports.postSignup = (req, res, next) => {
             subject: 'Signup succeeded!',
             html: '<h1>You successfully signed up!</h1>'
           })
-          .then(result => {
-            console.log(`After sending a request to send an email to ${email}`)
-            console.log(result)
-          })
-          .catch(err => console.log(err))
+            .then(result => {
+              console.log(`After sending a request to send an email to ${email}`)
+              console.log(result)
+            })
+            .catch(err => console.log(err))
         })
         .catch(err => console.log(err))
     })
@@ -129,5 +131,57 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err)
     res.redirect('/')
+  })
+}
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error')
+  console.log(message)
+  if (message.length > 0) {
+    message = message[0]
+  } else {
+    message = null
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  })
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err)
+      return res.redirect('/reset')
+    }
+    const token = buffer.toString('hex')
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', `No account with that '${req.body.email}' email found.`)
+          return res.redirect('/reset')
+        }
+        user.resetToken = token
+        user.resetTokenExpiration = Date.now() + 60 /*mins*/ * 60 /*secs*/ * 100 /*ms*/
+        return user.save()
+      })
+      .then(result => {
+        res.redirect('/')
+        sendgridMail.send({
+          to: req.body.email,
+          from: 'hoangtx.ict@gmail.com',
+          subject: 'Password Reset',
+          html: `
+            <p>You requested a password reset</p>
+            <p>Go to <a href="http://localhost:300/reset/${token}">this page</a> to set a new password.</p>
+          `
+        })
+        .then(result => {
+          console.log(`Sent a password reset guiding email to ${req.body.email}`)
+          console.log(`http://localhost:300/reset/${token}`)
+        })
+      })
+      .catch(err => console.log(err))
   })
 }
--- 01-online-shop/routes/auth.js
+++ 01-online-shop/routes/auth.js
@@ -9,5 +9,7 @@ route.get('/signup', authController.getSignup)
 route.post('/login', authController.postLogin)
 route.post('/signup', authController.postSignup)
 route.post('/logout', authController.postLogout)
+route.get('/reset', authController.getReset)
+route.post('/reset', authController.postReset)
 
 module.exports = route
