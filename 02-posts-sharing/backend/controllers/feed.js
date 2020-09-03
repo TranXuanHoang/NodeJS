4f8cab23456015c43e840d81d62e41d16acd9e40
TranXuanHoang <hoangtx.ict@gmail.com>
2020-09-03 23:48:18 +0900

    Update posts

--- 02-posts-sharing/backend/controllers/feed.js
+++ 02-posts-sharing/backend/controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 const { validationResult } = require('express-validator')
 
 const Post = require('../models/post')
@@ -30,7 +33,7 @@ exports.createPost = (req, res, next) => {
   }
 
   const { title, content } = req.body
-  const imageUrl = req.file.path.replace("\\" ,"/")
+  const imageUrl = req.file.path.replace('\\', '/')
   const post = new Post({
     title,
     content,
@@ -71,4 +74,60 @@ exports.getPost = (req, res, next) => {
       })
     })
     .catch(err => next(err))
+}
+
+exports.updatePost = (req, res, next) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    // Throw an error so that the control flow will reach to the error handler middleware
+    const error = new Error('Validation failed, entered data is incorrect.')
+    error.statusCode = 422
+    throw error
+  }
+  const postId = req.params.postId
+  const { title, content } = req.body
+  let imageUrl = req.body.image
+  if (req.file) {
+    imageUrl = req.file.path.replace('\\', '/')
+  }
+  if (!imageUrl) {
+    const error = new Error('No file picked.')
+    error.statusCode = 422
+    throw error
+  }
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        const error = new Error('Could not find post.')
+        error.statusCode = 404
+        throw error
+      }
+      // Delete old image if a new one was uploaded
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl)
+      }
+
+      // Update post data in the database
+      post.title = title
+      post.imageUrl = imageUrl
+      post.content = content
+      return post.save()
+    })
+    .then(result => {
+      res.status(200).json({
+        message: 'Post updated!',
+        post: result
+      })
+    })
+    .catch(err => next(err))
+}
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath)
+  fs.unlink(filePath, err => {
+    if (err) {
+      console.log(err)
+    }
+    console.log(`Deleted ${filePath}`)
+  })
 }
--- 02-posts-sharing/backend/routes/feed.js
+++ 02-posts-sharing/backend/routes/feed.js
@@ -5,18 +5,27 @@ const feedController = require('../controllers/feed')
 
 const router = express.Router()
 
+const POST_DATA_VALIDATORS = [
+  body('title').trim().isLength({ min: 5 }),
+  body('content').trim().isLength({ min: 5 })
+]
+
 // GET /feed/posts
 router.get('/posts', feedController.getPosts)
 
 // POST /feed/post
 router.post('/post',
-  [
-    body('title').trim().isLength({ min: 5 }),
-    body('content').trim().isLength({ min: 5 })
-  ],
+  POST_DATA_VALIDATORS,
   feedController.createPost
 )
 
+// GET /post/:postId
 router.get('/post/:postId', feedController.getPost)
+
+// PUT /post/:postId
+router.put('/post/:postId',
+  POST_DATA_VALIDATORS,
+  feedController.updatePost
+)
 
 module.exports = router
