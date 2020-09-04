47e3d3a80f171095874fedcf77303aa7294b97bd
TranXuanHoang <hoangtx.ict@gmail.com>
2020-09-04 10:45:46 +0900

    Paginate posts

--- 02-posts-sharing/backend/controllers/feed.js
+++ 02-posts-sharing/backend/controllers/feed.js
@@ -6,11 +6,23 @@ const { validationResult } = require('express-validator')
 const Post = require('../models/post')
 
 exports.getPosts = (req, res, next) => {
-  Post.find()
+  const currentPage = req.query.page || 1
+  const perPage = 3
+  let totalItems
+  Post.find().countDocuments()
+    .then(count => {
+      totalItems = count
+
+      // Fetch posts while paginating post items
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage)
+    })
     .then(posts => {
       res.status(200).json({
         message: 'Fetched posts successfully',
-        posts
+        posts,
+        totalItems
       })
     })
     .catch(err => next(err))
