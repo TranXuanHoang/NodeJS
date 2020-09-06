1df3ed4fd5ff69e869ed323e5217410460f67bc1
TranXuanHoang <hoangtx.ict@gmail.com>
2020-09-06 14:48:14 +0900

    Connect posts and users
    * Store a userId as creator of every post
    * The userId is obtained when login was successful

--- 02-posts-sharing/backend/controllers/feed.js
+++ 02-posts-sharing/backend/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path')
 const { validationResult } = require('express-validator')
 
 const Post = require('../models/post')
+const User = require('../models/user')
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1
@@ -46,20 +47,27 @@ exports.createPost = (req, res, next) => {
 
   const { title, content } = req.body
   const imageUrl = req.file.path.replace('\\', '/')
+  let creator
   const post = new Post({
     title,
     content,
     imageUrl,
-    creator: {
-      name: 'Hoang'
-    }
+    creator: req.userId
   })
   post.save()
     .then(result => {
-      console.log(result)
+      return User.findById(req.userId)
+    })
+    .then(user => {
+      creator = user
+      user.posts.push(post)
+      return user.save()
+    })
+    .then(result => {
       res.status(201).json({
         message: 'Post created successfully',
-        post: result
+        post: post,
+        creator: { _id: creator._id, name: creator.name }
       })
     })
     .catch(err => {
