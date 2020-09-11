dc58721e23e0c4151b98b1be16dc172e31971df6
TranXuanHoang <hoangtx.ict@gmail.com>
2020-09-11 18:21:42 +0900

    Change to use GraphQL to fetch posts
    * Define GraphQL schema and resolver for fetching posts API
    * Change frontend logic to using newly defined GraphQL API to fetch posts
    * Revise frontend logic of setting state after adding/updating a post

--- 02-posts-sharing/backend/graphql/resolvers.js
+++ 02-posts-sharing/backend/graphql/resolvers.js
@@ -106,5 +106,28 @@ module.exports = {
       createdAt: createdPost.createdAt.toISOString(),
       updatedAt: createdPost.updatedAt.toISOString()
     }
+  },
+
+  posts: async (args, req) => {
+    if (!req.isAuth) {
+      const error = new Error('Not authenticated.')
+      error.code = 401
+      throw error
+    }
+    const totalPosts = await Post.find().countDocuments()
+    const posts = await Post.find()
+      .sort({ createdAt: 'DESC' })
+      .populate('creator')
+
+    return {
+      posts: posts.map(p => {
+        return {
+          ...p._doc,
+          createdAt: p.createdAt.toISOString(),
+          updatedAt: p.updatedAt.toISOString()
+        }
+      }),
+      totalPosts
+    }
   }
 }
--- 02-posts-sharing/frontend/src/pages/Feed/Feed.js
+++ 02-posts-sharing/frontend/src/pages/Feed/Feed.js
@@ -54,26 +54,43 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch(`http://localhost:8080/feed/posts?page=${page}`, {
+    const graphqlQuery = {
+      query: `{
+        posts {
+          posts {
+            _id
+            title
+            content
+            creator { name }
+            createdAt
+          }
+          totalPosts
+        }
+      }`
+    }
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
       headers: {
-        Authorization: `Bearer ${this.props.token}`
-      }
+        Authorization: `Bearer ${this.props.token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(graphqlQuery)
     })
       .then(res => {
-        if (res.status !== 200) {
-          throw new Error('Failed to fetch posts.');
-        }
         return res.json();
       })
       .then(resData => {
+        if (resData.errors) {
+          throw new Error('Fetching posts failed!');
+        }
         this.setState({
-          posts: resData.posts.map(post => {
+          posts: resData.data.posts.posts.map(post => {
             return {
               ...post,
               imagePath: post.imageUrl
             }
           }),
-          totalPosts: resData.totalItems,
+          totalPosts: resData.data.totalPosts,
           postsLoading: false
         });
       })
@@ -141,11 +158,11 @@ class Feed extends Component {
             content: "${postData.content}",
             imageUrl: "${postData.image}"
           }) {
-            _id,
-            title,
-            content,
-            imageUrl,
-            creator { name },
+            _id
+            title
+            content
+            imageUrl
+            creator { name }
             createdAt
           }
         }
@@ -186,7 +203,17 @@ class Feed extends Component {
           createdAt: resData.data.createPost.createdAt
         };
         this.setState(prevState => {
+          let updatedPosts = [...prevState.posts];
+          if (prevState.editPost) {
+            const postIndex = prevState.posts.findIndex(
+              p => p._id === prevState.editPost._id
+            );
+            updatedPosts[postIndex] = post;
+          } else {
+            updatedPosts.unshift(post);
+          }
           return {
+            posts: updatedPosts,
             isEditing: false,
             editPost: null,
             editLoading: false
