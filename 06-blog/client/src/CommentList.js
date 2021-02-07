6f300eb81408b3eb098b16295907729f05b0c48e
TranXuanHoang <hoangtx.ict@gmail.com>
2021-02-07 14:04:42 +0900

    Switch to use post query microservice in fetching all posts and comments

--- 06-blog/client/src/CommentList.js
+++ 06-blog/client/src/CommentList.js
@@ -1,19 +1,6 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([])
-
-  const fetchData = useCallback(async () => {
-    const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-
-    setComments(res.data)
-  }, [postId])
-
-  useEffect(() => {
-    fetchData()
-  }, [fetchData])
-
+const CommentList = ({ comments }) => {
   const renderedComments = comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>
   })
