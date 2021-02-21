b81a452a3ea4fbdd65aec8fba4d52972ba1a90d3
TranXuanHoang <hoangtx.ict@gmail.com>
2021-02-22 00:22:56 +0900

    Add ClusterIP services for the posts and event-bus microservices
    * Define a new Kubernetes service called ClusterIP for each posts and event-bus microservices
    * Change the API endpoints used in these microservices to the name of these ClusterIP services

--- 06-blog/event-bus/index.js
+++ 06-blog/event-bus/index.js
@@ -13,7 +13,7 @@ app.post('/events', (req, res) => {
 
   events.push(event)
 
-  axios.post('http://localhost:4000/events', event)
+  axios.post('http://posts-clusterip-srv:4000/events', event)
     .catch((err) => {
       console.log(err.message);
     })
