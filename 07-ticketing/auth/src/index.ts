4a2cbb9dd1d9ed2cc55ffa22c560514d4bca2252
TranXuanHoang <hoangtx.ict@gmail.com>
2021-03-04 22:33:01 +0900

    Implement errors handler

--- 07-ticketing/auth/src/index.ts
+++ 07-ticketing/auth/src/index.ts
@@ -1,10 +1,11 @@
-import express from 'express'
 import { json } from 'body-parser'
-
+import express from 'express'
+import { errorHandler } from './middlewares/error-handler'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
+import { NotFoundError } from './errors/not-found-error'
 
 const app = express()
 app.use(json())
@@ -13,6 +14,13 @@ app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
+
+app.all('*', () => {
+  // Throw an error for any routes that are not found
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
 
 app.listen(3000, () => {
   console.log('[Auth] Listening on port 3000!')
