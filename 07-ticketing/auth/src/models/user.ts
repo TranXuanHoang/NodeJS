c35ee49970fcd66009e48b86fc71d61a9035adcf
TranXuanHoang <hoangtx.ict@gmail.com>
2021-03-07 01:41:37 +0900

    Define a User model

---
+++ 07-ticketing/auth/src/models/user.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+
+// An interface that describes the properties
+// that are required to create a new User
+interface UserAttrs {
+  email: string
+  password: string
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string
+  password: string
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    requited: true
+  },
+  password: {
+    type: String,
+    requited: true
+  }
+})
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs)
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
+
+export { User }
