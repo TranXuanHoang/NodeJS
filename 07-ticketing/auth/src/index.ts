2b1bdb42dc3a99360885d10d4313199004826afa
TranXuanHoang <hoangtx.ict@gmail.com>
2021-03-07 01:03:48 +0900

    Create and connect to a MongoDB for authentication microservice

--- 07-ticketing/auth/src/index.ts
+++ 07-ticketing/auth/src/index.ts
@@ -1,6 +1,7 @@
 import { json } from 'body-parser'
 import express from 'express'
 import 'express-async-errors' // handle error in async functions
+import mongoose from 'mongoose'
 import { NotFoundError } from './errors/not-found-error'
 import { errorHandler } from './middlewares/error-handler'
 import { currentUserRouter } from './routes/current-user'
@@ -23,6 +24,21 @@ app.all('*', async () => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log('[Auth] Listening on port 3000!')
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    console.log('Connected to MongoDB')
+  } catch (err) {
+    console.log(err)
+  }
+
+  app.listen(3000, () => {
+    console.log('[Auth] Listening on port 3000!')
+  })
+}
+
+start()
