d03263035489f5708d2fe7c51546e95754e6eb2b
TranXuanHoang <hoangtx.ict@gmail.com>
2021-03-07 23:24:40 +0900

    Hash passwords and use Mongoose pre-save hook

--- 07-ticketing/auth/src/models/user.ts
+++ 07-ticketing/auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import { Password } from '../services/password'
 
 // An interface that describes the properties
 // that are required to create a new User
@@ -29,6 +30,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     requited: true
   }
+})
+
+// pre('save', function Fn) means anytime we attempt to
+// save a document to the database collection, we are going to
+// execute the callback function Fn. Note that the callback function
+// is defined without using the arrow (=>) notation so that
+// inside of that callback function we can use 'this' to refer to
+// the actual document to be saved to the database. If we use the
+// arrow function notation, 'this' will be the context of the user.ts
+// file instead of the user documentation as supposed to be.
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'))
+    this.set('password', hashed)
+  }
+  done()
 })
 
 userSchema.statics.build = (attrs: UserAttrs) => {
