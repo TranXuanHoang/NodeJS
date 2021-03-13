b0f1e3010694b1611c2a7b2a2e849cd83f94bcf4
TranXuanHoang <hoangtx.ict@gmail.com>
2021-03-14 00:41:00 +0900

    Implement signout logic

--- 07-ticketing/auth/src/routes/signout.ts
+++ 07-ticketing/auth/src/routes/signout.ts
@@ -3,7 +3,15 @@ import express from 'express'
 const router = express.Router()
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('Hi there!')
+  // Send a HTTP header to tell the client browser to destroy the
+  // cookie that contains JSON Web Token used in authentication.
+  // https://www.npmjs.com/package/cookie-session#destroying-a-session
+  req.session = null
+
+  // NOTE: This signout has a flaw in which any fake cookies sent
+  // from client can make him/her signout
+
+  res.send({})
 })
 
 export { router as signoutRouter }
