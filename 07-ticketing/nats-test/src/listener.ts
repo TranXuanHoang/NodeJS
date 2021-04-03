61c769344778f47d82d97c04c79e8bc52a47ad60
TranXuanHoang <hoangtx.ict@gmail.com>
2021-04-03 15:04:15 +0900

    Allow multiple listeners to subscribe to NATS streaming server
    * Use different connection ID (randomly generated) to connect listeners to the NATS streaming server
    * Group same type of listeners into a NATS queue-group so that only one of them will receive a message at any point of time
    * Manually acknowledge NATS streaming server so that listeners can receive a broadcasted message again if any exception occured

--- 07-ticketing/nats-test/src/listener.ts
+++ 07-ticketing/nats-test/src/listener.ts
@@ -1,15 +1,24 @@
 import nats, { Message } from 'node-nats-streaming'
+import { randomBytes } from 'crypto'
 
 console.clear()
 
-const stan = nats.connect('ticketing', '123', {
+const connectId = randomBytes(4).toString('hex')
+const stan = nats.connect('ticketing', connectId, {
   url: 'http://localhost:4222'
 })
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS')
 
-  const subscription = stan.subscribe('ticket:created')
+  const options = stan.subscriptionOptions()
+    .setManualAckMode(true) // up to this listener to notify NATS the message has been received
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+    options
+  )
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData()
@@ -18,5 +27,9 @@ stan.on('connect', () => {
       console.log(`Received event #${msg.getSequence()}, with data:\n` +
         `${JSON.stringify(JSON.parse(data), null, 2)}`)
     }
+
+    // Acknowledge the NATS Streaming Server that the message was already
+    // received AND processed
+    msg.ack()
   })
 })
