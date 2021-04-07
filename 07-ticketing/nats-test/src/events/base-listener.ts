2350417287546d560d4316a32be3315e448aa71a
TranXuanHoang <hoangtx.ict@gmail.com>
2021-04-07 23:00:32 +0900

    Enforce data type and subject of events

--- 07-ticketing/nats-test/src/events/base-listener.ts
+++ 07-ticketing/nats-test/src/events/base-listener.ts
@@ -1,8 +1,14 @@
 import { Message, Stan } from "node-nats-streaming"
+import { Subjects } from "./subjects"
 
-export abstract class Listener {
+interface Event {
+  subject: Subjects
+  data: any
+}
+
+export abstract class Listener<T extends Event> {
   /** Name of the channel this listener is going to listen to. */
-  abstract subject: string
+  abstract subject: T['subject']
 
   /**
    * Name of the queue group this listener will join. NATS will send
@@ -11,7 +17,7 @@ export abstract class Listener {
   abstract queueGroupName: string
 
   /** Function to run when a message is received. */
-  abstract onMessage(data: any, msg: Message): void
+  abstract onMessage(data: T['data'], msg: Message): void
 
   /** Pre-initialized NATS client. */
   private client: Stan
--- 07-ticketing/nats-test/src/events/ticket-created-listener.ts
+++ 07-ticketing/nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,13 @@
 import { Message } from "node-nats-streaming"
 import { Listener } from './base-listener'
+import { Subjects } from "./subjects"
+import { TicketCreatedEvent } from "./ticket-created-event"
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created'
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated
   queueGroupName = 'payments-service'
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log(JSON.stringify(data, null, 2), "\n")
 
     // Acknowledge the NATS Streaming Server that the message was already
