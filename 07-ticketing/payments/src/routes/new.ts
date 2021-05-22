1e75871911d621d9b8d40d7a2a141f25dad75491
TranXuanHoang <hoangtx.ict@gmail.com>
2021-05-22 14:36:26 +0900

    Publish and listen for payment created event

--- 07-ticketing/payments/src/routes/new.ts
+++ 07-ticketing/payments/src/routes/new.ts
@@ -8,8 +8,10 @@ import {
 } from '@hoang-ticketing/common'
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher'
 import { Order } from '../models/order'
 import { Payment } from '../models/payment'
+import { natsWrapper } from '../nats-wrapper'
 import { stripe } from '../stripe'
 
 const router = express.Router()
@@ -55,8 +57,13 @@ router.post('/api/payments',
       stripeId: charge.id
     })
     await payment.save()
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId
+    })
 
-    res.status(201).send({ success: true })
+    res.status(201).send(payment)
   }
 )
 
