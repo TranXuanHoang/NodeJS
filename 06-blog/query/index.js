2f04fb694ebb725688ade1e9f27148ef32249d11
TranXuanHoang <hoangtx.ict@gmail.com>
2021-06-04 00:15:36 +0900

    Remove the use of body-parser

--- 06-blog/query/index.js
+++ 06-blog/query/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 const axios = require('axios')
 
 const app = express()
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(cors())
 
 const posts = {}
@@ -50,9 +49,13 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
   console.log('[Query Microservice] Listening on port 4002')
 
-  const res = await axios.get('http://event-bus-srv:4005/events')
+  try {
+    const res = await axios.get('http://event-bus-srv:4005/events')
 
-  for (let event of res.data) {
-    handleEvent(event.type, event.data)
+    for (let event of res.data) {
+      handleEvent(event.type, event.data)
+    }
+  } catch (error) {
+    console.log(error)
   }
 })
