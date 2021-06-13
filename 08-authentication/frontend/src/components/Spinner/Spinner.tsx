d1ccacc7434791dcc682ab3a4864eb5752eb43f4
TranXuanHoang <hoangtx.ict@gmail.com>
2021-06-14 02:20:21 +0900

    Add a spinner component

---
+++ 08-authentication/frontend/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,12 @@
+import styles from './Spinner.module.scss'
+
+const Spinner = () => (
+  <div className={styles.Spinner}>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+)
+
+export default Spinner
