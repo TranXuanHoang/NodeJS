f158cffb1cd9b807c5a5cf7f64ed8e7b6b593688
TranXuanHoang <hoangtx.ict@gmail.com>
2021-06-14 00:09:00 +0900

    Create signup form UI

--- 08-authentication/frontend/src/components/auth/Signup/Signup.tsx
+++ 08-authentication/frontend/src/components/auth/Signup/Signup.tsx
@@ -1,15 +1,62 @@
+import { Field, Form } from 'react-final-form'
+import { User } from '../authSlice'
 import styles from './Signup.module.scss'
 
+const someValuesUnset = (userCredential: User) => {
+  return !userCredential.email || !userCredential.password
+}
+
+const allvaluesUnset = (userCredential: User) => {
+  return !userCredential.email && !userCredential.password
+}
+
 const Signup = () => {
+  const onSubmit = (userCredential: User) => {
+    console.log(userCredential)
+  }
+
   return (
-    <form className={styles.Signup}>
-      <fieldset>
-        <label>Email</label>
-      </fieldset>
-      <fieldset>
-        <label>Password</label>
-      </fieldset>
-    </form>
+    <Form
+      // Will be passed in 'values' object as input argument
+      onSubmit={onSubmit}
+      initialValues={{ email: '', password: '' } as User}
+      // 'handleSubmit' = 'onSubmit'
+      render={({ handleSubmit, form, submitting, pristine, values }) => (
+        <form
+          className={styles.Signup}
+          autoComplete="off"
+          onSubmit={handleSubmit}>
+          <div className={styles.Header}>Register a New Account</div>
+          <label className={styles.EmailLabel}>Email</label>
+          <Field name="email"
+            component="input"
+            type="text"
+            placeholder="Email"
+            className={styles.EmailInput}
+          />
+          <label className={styles.PasswordLabel}>Password</label>
+          <Field name="password"
+            component="input"
+            type="password"
+            placeholder="Password"
+            className={styles.PasswordInput}
+          />
+          <div className={styles.ActionButtons}>
+            <button type="submit"
+              disabled={submitting || pristine || someValuesUnset(values)}
+            >
+              Sign Up
+            </button>
+            <button type="reset"
+              onClick={e => form.reset()}
+              disabled={submitting || pristine || allvaluesUnset(values)}
+            >
+              Reset
+            </button>
+          </div>
+        </form>
+      )}
+    />
   )
 }
 
