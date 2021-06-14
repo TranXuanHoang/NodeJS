4e83d8a27b0070b707062f155bb9047d069da45c
TranXuanHoang <hoangtx.ict@gmail.com>
2021-06-14 23:49:58 +0900

    Config CORS on the backend API server

--- 08-authentication/backend/src/app.ts
+++ 08-authentication/backend/src/app.ts
@@ -8,6 +8,26 @@ const app = express()
 // Log incoming requests
 app.use(morgan('combined'))
 
+// Config CORS
+// Note that, there is a 'cors' npm package library allowing simple and straightforward
+// configuration of CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+  // The graphqlHTTP middleware in
+  //     app.use('/graphql', graphqlHTTP(...))
+  // automatically reject any requests that are not POST.
+  // To allow OPTIONS requests (avoid CORS errors on clients),
+  // We immediately return a response with status of 200 when the request is a OPTIONS
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+
+  next()
+})
+
 // Parse incoming request body to JSON
 app.use(express.json())
 
